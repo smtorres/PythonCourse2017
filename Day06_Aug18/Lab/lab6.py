1ada5c2f6c946d80db1175082e2ad8adcfb5ed34
Michelle Torres <smtorres@wustl.edu>
2017-08-18 01:27:23 -0500

    Day 6: Material, slides, lab, exercises

---
+++ Day06_Aug18/Lab/lab6.py
@@ -0,0 +1,12 @@
+#Exercise 1
+#Write a function to calculate the greatest common divisor of two numbers
+
+#Exercise 2
+#Write a function that returns prime numbers less than 121
+
+#Exercise 3
+#Write a function that gives a solution to Tower of Hanoi game
+#https://www.mathsisfun.com/games/towerofhanoi.html
+
+
+
